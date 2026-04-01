afb643e7739668020bb7a6b703b2a0cd56ff2037
Nendra12 <ardhidanendra123@gmail.com>
2026-04-01 14:10:52 +0700

    Memperbaiki bug di landing page

--- src/components/NavbarLanding.jsx
+++ src/components/NavbarLanding.jsx
@@ -279,9 +279,9 @@ export default function NavbarLanding({ setActiveSection, activeSection }) {
                         </p>
                       </div>
                     </div>
-                    <button className="w-10 h-10 flex items-center justify-center bg-fourth rounded-md" onClick={navigate("/alumni/profile")}>
+                    <Link to={'/alumni/profile'} className="w-10 h-10 flex items-center justify-center bg-fourth rounded-md">
                       <UserPen size={24} />
-                    </button>
+                    </Link>
                   </div>
                 )}
                 {navLinks.map((item, i) => {
