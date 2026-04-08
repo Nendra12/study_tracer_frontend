acdb09a6e3107be7508f0bce508563e4fd937ff3
Nendra12 <ardhidanendra123@gmail.com>
2026-04-08 08:57:33 +0700

    Memperbaikan beranda alumni

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000/api";
-const API_BASE_URL = import.meta.env.VITE_API_URL || "https://api-tracer.primaesemkrada.com/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000/api";
+// const API_BASE_URL = import.meta.env.VITE_API_URL || "https://api-tracer.primaesemkrada.com/api";
 
 
 export const STORAGE_BASE_URL = import.meta.env.VITE_STORAGE_URL;
