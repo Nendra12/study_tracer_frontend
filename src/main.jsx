cf450a3936d29143160cd6f2aa55b6bdfd6ac05a
Wiwik Ainun Janah <esotreic@gmail.com>
2026-04-13 15:14:25 +0700

    Implement Google authentication for login and registration

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import { AuthProvider } from './context/AuthContext'
 import './style/index.css'
 import App from './App.jsx'
 import { ThemeProvider } from './context/ThemeContext.jsx'
 
+const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID
+
+const appTree = (
+  <BrowserRouter>
+      <AuthProvider>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </AuthProvider>
+  </BrowserRouter>
+)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-        <AuthProvider>
-          <ThemeProvider>
-            <App />
-          </ThemeProvider>
-        </AuthProvider>
-    </BrowserRouter>
+    {googleClientId ? (
+      <GoogleOAuthProvider clientId={googleClientId}>
+        {appTree}
+      </GoogleOAuthProvider>
+    ) : (
+      appTree
+    )}
   </StrictMode>,
 )
